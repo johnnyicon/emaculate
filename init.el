5e44ed499296946d0abc066d2c1eea54fcc219f9
John Gonzales <john@hashbang.co.nz>
2014-02-24 04:00:25 +1300

    Installed several packages and updated init.el - Installed magit - installed smex - installed find file in project - installed idle-highlight - installed ido-ubiquitous - installed goto-last-change - installed inf-ruby for ruby subprocesses - installed monokai-theme - Updated init.el - Remove better-defaults

--- init.el
+++ init.el
@@ -4,6 +4,20 @@
                          "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
-(require 'evil)
+;; (require 'evil)
 (evil-mode 1)
 
+(require 'color-theme)
+(setq color-theme-is-global t)
+
+(require 'smex)
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+(add-to-list 'load-path "~/.emacs.d/vendor/dirtree")
+(autoload 'dirtree "dirtree" "Add directory to tree view" t)
+
+(byte-recompile-directory "~/.emacs.d/vendor" 0)
