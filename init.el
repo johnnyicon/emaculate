c9912f4cbcf419c2b254b1c26d059a01f041a985
John Gonzales <john@hashbang.co.nz>
2014-02-23 19:16:17 +1300

    Installed color-theme Installed better-defaults Installed evil Installed undo-tree for evil

---
+++ init.el
@@ -0,0 +1,9 @@
+(require 'package)
+(add-to-list 'package-archives 
+                 '("marmalade" .
+                         "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(require 'evil)
+(evil-mode 1)
+
